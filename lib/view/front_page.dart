3a5c744256802d028d811aaca8db7fd821010aa4
hikaruueda <h_ueda@toridori.co.jp>
2022-12-04 23:02:50 +0900

    first commit

---
+++ lib/view/front_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class frontPage extends StatefulWidget {
+  const frontPage({super.key});
+
+  @override
+  State<frontPage> createState() => _frontPageState();
+}
+
+class _frontPageState extends State<frontPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
